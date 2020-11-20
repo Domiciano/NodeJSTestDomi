ca147170061b0f6d362eccb58d7bd22de778d36c
Domiciano <domiciano.rincon@correounivalle.edu.co>
2020-11-19 21:30:41 -0500

    Pre deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
  console.log(`Our app is running on port ${ PORT }`);
 });
 
